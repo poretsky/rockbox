84eb2ba74032eb2c28065ba008444bda4a99885a
Igor B. Poretsky <poretsky@mlbox.ru>
2015-02-25 18:54:39 +0300

    Shrink plugins list (inspired by FS#9043)

--- apps/filetypes.c
+++ apps/filetypes.c
@@ -193,9 +193,11 @@ static struct filetype_unknown unknown_file = {
 
 /* index array to filetypes used in open with list. */
 static int viewers[MAX_VIEWERS];
+static int viewers_selection[MAX_VIEWERS];
 static int filetype_count = 0;
 static unsigned char highest_attr = 0;
 static int viewer_count = 0;
+static int viewer_selection_count = 0;
 
 static int strdup_handle, strdup_bufsize, strdup_cur_idx;
 static int move_callback(int handle, void* current, void* new)
@@ -560,17 +562,17 @@ struct cb_data {
 static enum themable_icons openwith_get_icon(int selected_item, void * data)
 {
     (void)data;
-    return filetypes[viewers[selected_item]].icon;
+    return filetypes[viewers_selection[selected_item]].icon;
 }
 
 static const char* openwith_get_name(int selected_item, void * data,
                                      char * buffer, size_t buffer_len)
 {
     (void)data; (void)buffer; (void)buffer_len;
-    const char *s = strrchr(filetypes[viewers[selected_item]].plugin, '/');
+    const char *s = strrchr(filetypes[viewers_selection[selected_item]].plugin, '/');
     if (s)
         return s+1;
-    else return filetypes[viewers[selected_item]].plugin;
+    else return filetypes[viewers_selection[selected_item]].plugin;
 }
 
 static int openwith_get_talk(int selected_item, void * data)
@@ -578,7 +580,7 @@ static int openwith_get_talk(int selected_item, void * data)
     (void)data;
     char viewer_filename[MAX_FILENAME];
     snprintf(viewer_filename, MAX_FILENAME, "%s.%s",
-             filetypes[viewers[selected_item]].plugin, ROCK_EXTENSION);
+             filetypes[viewers_selection[selected_item]].plugin, ROCK_EXTENSION);
     talk_file_or_spell(PLUGIN_DIR, viewer_filename,
                        NULL, false);
     return 0;
@@ -591,7 +593,7 @@ static int openwith_action_callback(int action, struct gui_synclist *lists)
     if (action == ACTION_STD_OK)
     {
         char plugin[MAX_PATH];
-        i = viewers[gui_synclist_get_sel_pos(lists)];
+        i = viewers_selection[gui_synclist_get_sel_pos(lists)];
         snprintf(plugin, MAX_PATH, "%s/%s.%s",
                     PLUGIN_DIR, filetypes[i].plugin, ROCK_EXTENSION);
         plugin_load(plugin, info->current_file);
@@ -604,14 +606,41 @@ int filetype_list_viewers(const char* current_file)
 {
     struct simplelist_info info;
     struct cb_data data = { current_file };
+    int i;
+
+    viewer_selection_count = 0;
+
+    /* do not show unrelated plugins, unless 'show files' is 'all' */
+    if (global_settings.dirfilter != 0)
+    {
+        char *extension = strrchr(current_file, '.');
+        if (extension)
+            extension++;
+        for (i = 1; i < filetype_count; i++)
+            if (filetypes[i].plugin && (viewer_selection_count < MAX_VIEWERS) &&
+                ((!strcmp(filetypes[i].extension, "*")) ||
+                 (!strcasecmp(filetypes[i].extension, extension))))
+            {
+                int j;
+                for (j = 0; j < viewer_selection_count; j++)
+                    if (!strcmp(filetypes[viewers_selection[j]].plugin, filetypes[i].plugin))
+                        break;
+                if (j == viewer_selection_count)
+                    viewers_selection[viewer_selection_count++] = i;
+            }
+    }
+    else
+        for (i = 0; i < viewer_count; i++)
+            viewers_selection[viewer_selection_count++] = viewers[i];
+
 #ifndef HAVE_LCD_BITMAP
-    if (viewer_count == 0)
+    if (viewer_selection_count == 0)
     {
         splash(HZ*2, ID2P(LANG_NO_VIEWERS));
         return PLUGIN_OK;
     }
 #endif
-    simplelist_info_init(&info, str(LANG_ONPLAY_OPEN_WITH), viewer_count, &data);
+    simplelist_info_init(&info, str(LANG_ONPLAY_OPEN_WITH), viewer_selection_count, &data);
     info.action_callback = openwith_action_callback;
     info.get_name = openwith_get_name;
     info.get_icon = global_settings.show_icons?openwith_get_icon:NULL;
