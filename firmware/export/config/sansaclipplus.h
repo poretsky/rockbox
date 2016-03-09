8b2fbfd651fa0354e842aa5221f77690e72f017f
Igor B. Poretsky <poretsky@mlbox.ru>
2016-03-09 23:51:48 +0300

    Corrected run time prediction

--- firmware/export/config/sansaclipplus.h
+++ firmware/export/config/sansaclipplus.h
@@ -173,7 +173,7 @@
 #define HAVE_POWEROFF_WHILE_CHARGING
 
 /* define current usage levels (based on battery bench) */
-#define CURRENT_NORMAL     11
+#define CURRENT_NORMAL     12
 #define CURRENT_BACKLIGHT  15
 #define CURRENT_RECORD     CURRENT_NORMAL /* TODO */
 
