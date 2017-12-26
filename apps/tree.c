03c2084802204d0a599dd286f4015b612c6c0df0
Igor B. Poretsky <poretsky@mlbox.ru>
2017-12-26 07:13:11 +0300

    Don't reload directory if it is actually not changed

--- apps/tree.c
+++ apps/tree.c
@@ -363,7 +363,7 @@ static int update_dir(void)
     {
         tc.sort_dir = global_settings.sort_dir;
         /* if the tc.currdir has been changed, reload it ...*/
-        if (strncmp(tc.currdir, lastdir, sizeof(lastdir)) || reload_dir)
+        if (strcmp(tc.currdir, lastdir) || reload_dir)
         {
             if (ft_load(&tc, NULL) < 0)
                 return -1;
@@ -583,7 +583,7 @@ void set_current_file(const char *path)
 
     /* If we changed dir we must recalculate the dirlevel
        and adjust the selected history properly */
-    if (strncmp(tc.currdir,lastdir,sizeof(lastdir)))
+    if (strcmp(tc.currdir, lastdir))
     {
         tc.dirlevel =  0;
         tc.selected_item_history[tc.dirlevel] = -1;
