8dc36e13a9983e16f72aa051cfd45c433329f630
Igor B. Poretsky <poretsky@mlbox.ru>
2018-09-27 22:31:15 +0300

    Only charging via USB option

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -1942,6 +1942,10 @@ const struct settings_list settings[] = {
                    "off,on,force", NULL, 3, ID2P(LANG_SET_BOOL_NO),
                    ID2P(LANG_SET_BOOL_YES), ID2P(LANG_FORCE)),
 #endif
+#if defined(HAVE_USB_POWER) && !defined(USB_NONE) && !defined(SIMULATOR)
+    OFFON_SETTING(0, usb_charge_only, LANG_USB_CHARGE_ONLY, false,
+                  "usb charge only", usb_set_charge_setting),
+#endif
     OFFON_SETTING(F_BANFROMQS,cuesheet,LANG_CUESHEET_ENABLE,false,"cuesheet support",
                   NULL),
     TABLE_SETTING(F_ALLOW_ARBITRARY_VALS, skip_length,
