a47de3413dbb1b5d66244845e21971fa96b3f5ab
Igor B. Poretsky <poretsky@mlbox.ru>
2020-09-09 09:58:54 +0300

    Plugins tree browsing restriction
    Don't leave the subtree and show only appropriate files.

--- apps/tree.c
+++ apps/tree.c
@@ -964,7 +964,7 @@ int rockbox_browse(struct browse_context *browse)
     tc.sort_dir = global_settings.sort_dir;
 
     reload_dir = true;
-    if (*tc.dirfilter >= NUM_FILTER_MODES)
+    if (*tc.dirfilter >= NUM_FILTER_MODES && *tc.dirfilter != SHOW_PLUGINS_TREE)
     {
         int last_context;
         /* don't reset if its the same browse already loaded */
@@ -995,11 +995,14 @@ int rockbox_browse(struct browse_context *browse)
     }
     else
     {
-        if (dirfilter != SHOW_ID3DB)
-            tc.dirfilter = &global_settings.dirfilter;
         tc.browse = browse;
         strlcpy(current, browse->root, MAX_PATH);
         set_current_file(current);
+        if (dirfilter == SHOW_PLUGINS_TREE)
+            tc.dirlevel = 0; 
+        else if (dirfilter != SHOW_ID3DB)
+            tc.dirfilter = &global_settings.dirfilter;
+
         if (browse->flags&BROWSE_RUNFILE)
             ret_val = ft_enter(&tc);
         else
