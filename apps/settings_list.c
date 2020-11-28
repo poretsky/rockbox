44f28b2f01d9f866ab67b6643fdbbc2bb522ad6c
Igor B. Poretsky <poretsky@mlbox.ru>
2020-11-28 17:28:16 +0300

    FS#11541 - Add Voice Announcement of Summary Info to WPS hotkey
    voice-hotkey-2010-12-12.patch synchronized and fixed by me.
    
    This setting is meaningless without a hotkey itself.

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -1470,6 +1470,10 @@ const struct settings_list settings[] = {
     INT_SETTING(0, talk_mixer_amp, LANG_TALK_MIXER_LEVEL, 100,
         "talk mixer level", UNIT_PERCENT, 0, 100, 5, NULL, NULL, voice_set_mixer_level),
 
+#ifdef HAVE_HOTKEY
+    TEXT_SETTING(0, wps_announcement_format, "wps announcement format", "", NULL, NULL),
+#endif
+
 #ifdef HAVE_RECORDING
      /* recording */
     TABLE_SETTING(F_TIME_SETTING | F_ALLOW_ARBITRARY_VALS | F_RECSETTING,
@@ -2189,10 +2193,10 @@ const struct settings_list settings[] = {
 #ifdef HAVE_HOTKEY
     TABLE_SETTING(F_CB_ON_SELECT_ONLY, hotkey_wps,
         LANG_HOTKEY_WPS, HOTKEY_VIEW_PLAYLIST, "hotkey wps",
-        "off,view playlist,show track info,pitchscreen,open with,delete,bookmark,plugin,bookmark list"
-        ,UNIT_INT, hotkey_formatter, hotkey_getlang, hotkey_callback,9, HOTKEY_OFF,
+        "off,view playlist,show track info,pitchscreen,open with,delete,bookmark,plugin,bookmark list,voiceinfo"
+        ,UNIT_INT, hotkey_formatter, hotkey_getlang, hotkey_callback, 10, HOTKEY_OFF,
         HOTKEY_VIEW_PLAYLIST, HOTKEY_SHOW_TRACK_INFO, HOTKEY_PITCHSCREEN,
-        HOTKEY_OPEN_WITH, HOTKEY_DELETE, HOTKEY_BOOKMARK, HOTKEY_PLUGIN, HOTKEY_BOOKMARK_LIST),
+        HOTKEY_OPEN_WITH, HOTKEY_DELETE, HOTKEY_BOOKMARK, HOTKEY_PLUGIN, HOTKEY_BOOKMARK_LIST, HOTKEY_VOICEINFO),
     TABLE_SETTING(0, hotkey_tree,
         LANG_HOTKEY_FILE_BROWSER, HOTKEY_OFF, "hotkey tree",
 #ifdef HAVE_TAGCACHE
