poretsky/rockbox

commit 017dd72ff3f787f2c818530293656658d338aa2f

plugins:  Convert all plugins to using mixer API for playback
pcm_play_data -> mixer_channel_play_data
pcm_set_frequeny -> mixer_set_frequency
pcm_play_stop -> audio_stop at startup / mixer_channel_stop at shutdown
pcm_is_playing -> mixer_channel_status

All of these have been removed from the plugin API.  Updated API docs to
clarify role of audio_stop() vs mixer_channel_stop()

Todo:  get rid of pcm_play_[un]lock().

Change-Id: I8800c131b51f064ee923a4c6e42aa405d33851fc

firmware/pcm_mixer.c
@@ -455,6 +455,10 @@ void mixer_set_frequency(unsigned int samplerate)
     pcm_set_frequency(samplerate);
     samplerate = pcm_get_frequency();
 
+#ifdef CONFIG_SAMPR_TYPES
+    samplerate &= ~SAMPR_TYPE_MASK;
+#endif
+
     if (samplerate == mixer_sampr)
         return;
 

apps/plugin.c
@@ -645,10 +645,6 @@ static const struct plugin_api rockbox_api = {
     &audio_master_sampr_list[0],
     &hw_freq_sampr[0],
     pcm_apply_settings,
-    pcm_play_data,
-    pcm_play_stop,
-    pcm_set_frequency,
-    pcm_is_playing,
     pcm_play_lock,
     pcm_play_unlock,
     beep_play,

apps/plugin.h
@@ -178,7 +178,7 @@ int plugin_open(const char *plugin, const char *parameter);
  * when this happens please take the opportunity to sort in
  * any new functions "waiting" at the end of the list.
  */
-#define PLUGIN_API_VERSION 278
+#define PLUGIN_API_VERSION 279
 
 /* 239 Marks the removal of ARCHOS HWCODEC and CHARCELL */
 
@@ -743,12 +743,6 @@ struct plugin_api {
     const unsigned long *audio_master_sampr_list;
     const unsigned long *hw_freq_sampr;
     void (*pcm_apply_settings)(void);
-    void (*pcm_play_data)(pcm_play_callback_type get_more,
-                          pcm_status_callback_type status_cb,
-                          const void *start, size_t size);
-    void (*pcm_play_stop)(void);
-    void (*pcm_set_frequency)(unsigned int frequency);
-    bool (*pcm_is_playing)(void);
     void (*pcm_play_lock)(void);
     void (*pcm_play_unlock)(void);
     void (*beep_play)(unsigned int frequency, unsigned int duration,

apps/plugins/doom/i_sound.c
@@ -471,13 +471,13 @@ void I_SubmitSound(void)
    if (!enable_sound)
       return;
 
-   rb->pcm_play_data(&get_more, NULL, NULL, 0);
+   rb->mixer_channel_play_data(PCM_MIXER_CHAN_PLAYBACK, &get_more, NULL, 0);
 }
 
 void I_ShutdownSound(void)
 {
-   rb->pcm_play_stop();
-   rb->pcm_set_frequency(HW_SAMPR_DEFAULT); // 44100
+   rb->mixer_channel_stop(PCM_MIXER_CHAN_PLAYBACK);
+   rb->mixer_set_frequency(HW_SAMPR_DEFAULT);
 }
 
 void I_InitSound()
@@ -486,14 +486,14 @@ void I_InitSound()
 
    // Initialize external data (all sounds) at start, keep static.
    printf( "I_InitSound: ");
-   rb->pcm_play_stop();
+   rb->audio_stop();
 
 #if INPUT_SRC_CAPS != 0
    /* Select playback */
    rb->audio_set_input_source(AUDIO_SRC_PLAYBACK, SRCF_PLAYBACK);
    rb->audio_set_output_source(AUDIO_SRC_PLAYBACK);
 #endif
-   rb->pcm_set_frequency(SAMPLERATE);
+   rb->mixer_set_frequency(SAMPLERATE);
 
    vol_lookup=malloc(128*256*sizeof(int));
 

apps/plugins/imageviewer/imageviewer.c
@@ -342,7 +342,7 @@ static int show_menu(void) /* return 1 to quit */
             /* slideshow times < 10s keep disk spinning */
             rb->storage_spindown(0);
         }
-        else if (!rb->pcm_is_playing())
+        else if (rb->mixer_channel_status(PCM_MIXER_CHAN_PLAYBACK) != CHANNEL_PLAYING)
         {
             /* slideshow times > 10s and not playing: ata_off after load */
             iv_api.immediate_ata_off = true;

apps/plugins/lua/include_lua/pcm.lua
@@ -25,8 +25,5 @@
 if not rb.pcm then rb.splash(rb.HZ, "No Support!") return nil end
 
 rb.pcm_apply_settings = function() rb.pcm("apply_settings") end
-rb.pcm_set_frequency = function(freq) rb.pcm("set_frequency", freq) end
-rb.pcm_play_stop = function() rb.pcm("play_stop") end
 rb.pcm_play_lock = function() rb.pcm("play_lock") end
 rb.pcm_play_unlock = function() rb.pcm("play_unlock") end
-rb.pcm_is_playing = function() return rb.pcm("is_playing") end

apps/plugins/lua/rocklib.c
@@ -549,13 +549,11 @@ RB_WRAP(sound)
 
 RB_WRAP(pcm)
 {
-    enum e_pcm {PCM_APPLYSETTINGS = 0, PCM_ISPLAYING,
-                PCM_PLAYSTOP, PCM_PLAYLOCK, PCM_PLAYUNLOCK,
-                PCM_SETFREQUENCY, PCM_ECOUNT};
+    enum e_pcm {PCM_APPLYSETTINGS = 0, PCM_PLAYLOCK, PCM_PLAYUNLOCK,
+                PCM_ECOUNT};
 
-    const char *pcm_option[] = {"apply_settings", "is_playing",
-                                "play_stop", "play_lock", "play_unlock",
-                                "set_frequency", NULL};
+    const char *pcm_option[] = {"apply_settings", "play_lock", "play_unlock",
+                                NULL};
     bool   b_result;
 
     lua_pushnil(L); /*push nil so options w/o return have something to return */
@@ -566,21 +564,11 @@ RB_WRAP(pcm)
         case PCM_APPLYSETTINGS:
             rb->pcm_apply_settings();
             break;
-        case PCM_ISPLAYING:
-            b_result = rb->pcm_is_playing();
-            lua_pushboolean(L, b_result);
-            break;
-        case PCM_PLAYSTOP:
-            rb->pcm_play_stop();
-            break;
         case PCM_PLAYLOCK:
             rb->pcm_play_lock();
             break;
         case PCM_PLAYUNLOCK:
             rb->pcm_play_unlock();
-            break;
-        case PCM_SETFREQUENCY:
-            rb->pcm_set_frequency((unsigned int) luaL_checkint(L, 2));
             break;
     }
 

apps/plugins/metronome.c
@@ -809,9 +809,6 @@ static int bpm_step_counter = 0;
 
 static bool sound_trigger = false;
 
-#define MET_IS_PLAYING rb->pcm_is_playing()
-#define MET_PLAY_STOP rb->audio_stop()
-
 /* Really necessary? Cannot just play mono?
    Also: This is wasted memory! */
 static short tick_buf[sizeof(tick_sound)*2];
@@ -829,12 +826,12 @@ static void prepare_buffers(void)
 
 static void play_tick(void)
 {
-    rb->pcm_play_data(NULL, NULL, tick_buf, sizeof(tick_buf));
+    rb->mixer_channel_play_data(PCM_MIXER_CHAN_PLAYBACK, NULL, tick_buf, sizeof(tick_buf));
 }
 
 static void play_tock(void)
 {
-    rb->pcm_play_data(NULL, NULL, tock_buf, sizeof(tock_buf));
+    rb->mixer_channel_play_data(PCM_MIXER_CHAN_PLAYBACK, NULL, tock_buf, sizeof(tock_buf));
 }
 
 /* State: 0: blank/title, 1: tick, 2: tock 3: silent klick */
@@ -1216,10 +1213,10 @@ static void cleanup(void)
     if(fd >= 0) rb->close(fd);
 
     metronome_pause();
-    MET_PLAY_STOP; /* stop audio ISR */
+    rb->mixer_channel_stop(PCM_MIXER_CHAN_PLAYBACK);
     tweak_volume(0);
     rb->led(0);
-    rb->pcm_set_frequency(HW_SAMPR_DEFAULT);
+    rb->mixer_set_frequency(HW_SAMPR_DEFAULT);
 }
 
 /*
@@ -1555,7 +1552,7 @@ enum plugin_status plugin_start(const void* file)
 
     mem_init();
 
-    if(MET_IS_PLAYING) MET_PLAY_STOP; /* stop audio IS */
+    rb->audio_stop();
 
     prepare_buffers();
 #if INPUT_SRC_CAPS != 0
@@ -1563,7 +1560,7 @@ enum plugin_status plugin_start(const void* file)
     rb->audio_set_input_source(AUDIO_SRC_PLAYBACK, SRCF_PLAYBACK);
     rb->audio_set_output_source(AUDIO_SRC_PLAYBACK);
 #endif
-    rb->pcm_set_frequency(SAMPR_44);
+    rb->mixer_set_frequency(SAMPR_44);
 
     if(file)
     {

commit d1abd788d0b22de1d8165b01b8c69eb6cb1ccd2d

pcm_mixer: fix missing NULL check for play_cbs
Fixes Metronome plugin crashing
(regression introduced in cb04b81)

Does *not* fix missing Metronome sound
(regression introduced in 017dd72)

Change-Id: I1da9aa2c937b267a3d5b122c431eaa9f7e748440

firmware/pcm_mixer.c
@@ -163,7 +163,7 @@ fill_frame:
 
         if (chan->size == 0)
         {
-            if (chan->play_cbs->get_more)
+            if (chan->play_cbs && chan->play_cbs->get_more)
             {
                 chan->play_cbs->get_more(&chan->start, &chan->size);
                 ALIGN_AUDIOBUF(chan->start, chan->size);